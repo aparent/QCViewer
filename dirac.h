a47aab1865a2c74f4f2cb30dd4b0660e72475c3e
Jacob Parker <j3parker@uwaterloo.ca>
2011-05-22 20:46:41 -0400

    glueing dirac parsing

--- dirac.h
+++ dirac.h
@@ -15,6 +15,13 @@ struct diracTerm{
 	cx_double numValue; 
 };
 
+struct stateVec {
+	unsigned int dim;
+  complex<float> *data;
+}
+
 parseNode *parseDirac(string input); //Defined in diracParser.y
 string printTree(parseNode *node);
 diracTerm evalTree(parseNode *node);
+
+stateVec getStateVec (string input);
