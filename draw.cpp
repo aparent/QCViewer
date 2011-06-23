aparent/QCViewer

commit 68b96067ef9a17e10927482454e89dd6d8eb232b

fixed up draw bugs, improved stateview

draw.cpp
@@ -157,21 +157,21 @@ gateRect drawCU (cairo_t *cr, unsigned int xc, string name, vector<Control> *ctr
   if (width < dw*Upad) {
     width = dw*Upad;
   }
-  cairo_rectangle (cr, xc-width/2, yc-height/2, width, height);
+  cairo_rectangle (cr, xc-radius, yc-height/2, width, height);
   cairo_set_source_rgb (cr, 1, 1, 1);
   cairo_fill(cr);
-  cairo_rectangle (cr, xc-width/2, yc-height/2, width, height);
+  cairo_rectangle (cr, xc-radius, yc-height/2, width, height);
   cairo_set_source_rgb (cr, 0, 0, 0);
   cairo_set_line_width (cr, thickness);
   cairo_stroke(cr);
 
-  double x = xc - (extents.width/2 + extents.x_bearing);
-  double y = yc - (extents.height/2 + extents.y_bearing);
+  double x = (xc - radius + width/2) - extents.width/2 - extents.x_bearing;
+  double y = yc - extents.height/2 - extents.y_bearing;
   cairo_move_to(cr, x, y);
   cairo_show_text (cr, name.c_str());
   gateRect r;
-  r.x0 = xc - width/2 - thickness;
-  r.y0 = yc - height/2 - thickness;
+  r.x0 = xc - thickness-radius;
+  r.y0 = yc -height/2 - thickness;
   r.width = width + 2*thickness;
   r.height = height + 2*thickness;
   return combine_gateRect(rect, r);
@@ -332,15 +332,11 @@ vector<gateRect> draw (cairo_t *cr, Circuit* c, vector<LayoutColumn>& columns, d
   // Draw them in parallel using the greedy strategy.
   unsigned int i = 0;
   if (columns.size () == 0) cout << "WARNING: invalid layout detected in " << __FILE__ << " at line " << __LINE__ << "!\n";
-  double maxwidth = 0.0;
   for (unsigned int j = 0; j < columns.size(); j++) {
-		xcurr += maxwidth;
-		maxwidth = 0.0;
     for (; i <= columns[j].lastGateID; i++) {
       Gate* g = c->getGate (i);
       gateRect r;
       minmaxWire (&g->controls, &g->targets, &mingw, &maxgw);
-      int count = 1; // XXX: hack for clumping hadamards, remove soon
       switch (g->drawType) {
         case Gate::NOT: r = drawCNOT (cr, xcurr, &g->controls, &g->targets); break;
         case Gate::FRED: r = drawFred (cr, xcurr, &g->controls, &g->targets); break;
@@ -374,13 +370,14 @@ vector<gateRect> draw (cairo_t *cr, Circuit* c, vector<LayoutColumn>& columns, d
           }
 					break;
        }
-       for (int i = 0; i < count; i++) rects.push_back(r);
-       maxwidth = max (maxwidth, r.width);
+       rects.push_back(r);
+       xcurr += r.width;
     //  drawRect (cr, r, Colour (0.1,0.5,0.2,0.8), Colour (0.1, 0.5, 0.2, 0.3)); // DEBUG
     }
     xcurr += gatePad + columns[j].pad;
   }
-  *wireend = xcurr+gatePad;
+  xcurr -= gatePad;
+  *wireend = xcurr;
 
   // output labels
   cairo_set_source_rgb (cr, 0, 0, 0);
@@ -461,7 +458,7 @@ vector<gateRect> draw_circuit (Circuit *c, cairo_t* cr, vector<LayoutColumn>& co
   cairo_set_font_size(cr, 18);
 
   vector<gateRect> rects;
-  drawbase (cr, c, ext.width+ext.x, ext.height+scale*ext.y+thickness, wirestart+xoffset, wireend, scale);
+  drawbase (cr, c, ext.width+ext.x, ext.height+scale*ext.y+thickness, wirestart, wireend+xoffset, scale);
   rects = draw (cr, c, columns, &wirestart, &wireend, true);
   if (drawParallel) drawParallelSectionMarkings (cr, rects, c->numLines(),c->getParallel());
   if (drawArch) drawArchitectureWarnings (cr, rects, c->getArchWarnings());

stateWidget.cpp
@@ -75,7 +75,7 @@ StateWidget::StateWidget(Gtk::Statusbar* ns) {
 }
 
 // TODO: this is ugly
-#define EPS 0.000001
+#define EPS 0.0001
 
 bool StateWidget::on_expose_event (GdkEventExpose* event) {
   (void)event; // placate compiler...
@@ -135,13 +135,14 @@ bool StateWidget::on_expose_event (GdkEventExpose* event) {
       if (state != NULL) {
         double maxX = 0.0;
         for (unsigned int i = 0; i < num_draw; i++) {
-          double val = drawmode == STATEDRAW_REAL ? bucket[i].real() : bucket[i].imag ();
-          avg += val;
+          double val = max(bucket[i].real(), bucket[i].imag ());
+          avg += drawmode == STATEDRAW_REAL ? bucket[i].real () : bucket[i].imag ();
           maxX = max (abs(val), maxX);
         }
         for (unsigned int i = 0; i < num_draw; i++) {
-          float_t eValue = (drawmode == STATEDRAW_REAL ? bucket[i].real() : bucket[i].imag ())/maxX;
-          if (abs(eValue) > EPS) {
+          float_t val = drawmode == STATEDRAW_REAL ? bucket[i].real() : bucket[i].imag ();
+          float_t eValue = val/maxX;
+          if (abs(val) > EPS) {
             cr->rectangle (xborder + (double)i*barWidth, height/2.0, barWidth, -eValue*barHeight/2.0);
             cr->set_source_rgb (0, 0, 0);
              cr->stroke_preserve ();
@@ -199,10 +200,15 @@ bool StateWidget::onMotionEvent (GdkEventMotion* event) {
     string mystr;
     int dim = 0;
 		if (drawmode!=STATEDRAW_EXPECTED_TRACED){
-    	double eValue = bucket[i].real()*bucket[i].real() + bucket[i].imag()*bucket[i].imag();
+      double rValue = bucket[i].real ();
+      double iValue = bucket[i].imag ();
+      rValue = abs(rValue) > EPS ? rValue : 0;
+      iValue = abs(iValue) > EPS ? iValue : 0;
+    	double eValue = rValue*rValue + iValue*iValue;
+      eValue = abs(eValue) > EPS ? eValue : 0;
     	int twotothedim = state->dim;
     	while (twotothedim >>= 1) dim++; // TODO: we should have size/dim encoded in the state vector.
-			oss << "State " << base2enc ((unsigned long)bucketID[i], dim) << " (" << bucketID[i] << ") amplitude " << bucket[i].real() << " + " << bucket[i].imag () << "i (" << 100.0*eValue << "%)";
+			oss << "State " << base2enc ((unsigned long)bucketID[i], dim) << " (" << bucketID[i] << ") amplitude " << rValue << " + " << iValue << "i (" << 100.0*eValue << "%)";
 		} else{
     	unsigned int twotothedim = num_draw_traced;
     	while (twotothedim >>= 1) dim++; // TODO: we should have size/dim encoded in the state vector.

testCircuits/mod5d4.tfc
@@ -8,4 +8,6 @@ t2 "this is a really long label",test123
 t3 c,test123,e
 t2 c,test123
 t2 test123,e
+ABCDEF test123
+H test123
 END

window.cpp
@@ -438,7 +438,6 @@ void QCViewer::on_menu_delete () {
 }
 
 void QCViewer::set_selection (int i) {
-	cout << "selection was " << selection << " now is " << i << endl;
   selection = i;
   if (i == -1) {
     btn_editcontrols.set_active (false);

commit 5da931a28d194872558a1bcb298d37d28f3e5bd8

fixed bug introduced in  68b96067ef9a17e10927482454e89dd6d8eb232b

draw.cpp
@@ -330,8 +330,10 @@ vector<gateRect> draw (cairo_t *cr, Circuit* c, vector<LayoutColumn>& columns, d
 // TODO: remove  vector <int> parallels = c->getGreedyParallel ();
 
   unsigned int i = 0;
+  double maxX;
   if (columns.size () == 0) cout << "WARNING: invalid layout detected in " << __FILE__ << " at line " << __LINE__ << "!\n";
   for (unsigned int j = 0; j < columns.size(); j++) {
+    maxX = 0.0;
     for (; i <= columns[j].lastGateID; i++) {
       Gate* g = c->getGate (i);
       gateRect r;
@@ -370,9 +372,10 @@ vector<gateRect> draw (cairo_t *cr, Circuit* c, vector<LayoutColumn>& columns, d
 					break;
        }
        rects.push_back(r);
-       xcurr += r.width;
+       maxX = max (maxX, r.width);
     //  drawRect (cr, r, Colour (0.1,0.5,0.2,0.8), Colour (0.1, 0.5, 0.2, 0.3)); // DEBUG
     }
+    xcurr += maxX;
     xcurr += gatePad + columns[j].pad;
   }
   xcurr -= gatePad;