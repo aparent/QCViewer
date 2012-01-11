5d19e3c1cd9d30e5d8dec6b34256ab5d3d250908
Alex Parent <aparent@uwaterloo.ca>
2012-01-11 12:52:24 -0500

    Adding license notices to all code

--- src/QCLib/simulate.cpp
+++ src/QCLib/simulate.cpp
@@ -1,3 +1,30 @@
+
+/*--------------------------------------------------------------------
+QCViewer
+Copyright (C) 2011  Alex Parent and The University of Waterloo,
+Institute for Quantum Computing, Quantum Circuits Group
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+See also 'ADDITIONAL TERMS' at the end of the included LICENSE file.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+QCViewer is a trademark of the of the The University of Waterloo,
+Institute for Quantum Computing, Quantum Circuits Group
+
+Authors: Alex Parent, Jakub Parker
+---------------------------------------------------------------------*/
+
 #include "simulate.h"
 #include <cmath>
 #include <iostream>
